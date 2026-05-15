57d99ff54a10c9b0d28ad2cce4f8c65cea791328
Bhavna Ramkumar <bhavna@bhavnas-mbp-2.lan>
2026-05-15 13:49:12 -0700

    fix: Add check for missing env vars on login

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -10,6 +10,10 @@ export const actions = {
 		console.log('got:', JSON.stringify(username), JSON.stringify(password));
 		console.log('exp:', JSON.stringify(env.ADMIN_USERNAME), JSON.stringify(env.ADMIN_PASSWORD));
 
+		if (!env.ADMIN_USERNAME || !env.ADMIN_PASSWORD) {
+			return fail(500, { error: 'Server configuration error: Missing environment variables on Cloudflare.' });
+		}
+
 		if (username !== env.ADMIN_USERNAME || password !== env.ADMIN_PASSWORD) {
 			return fail(401, { error: 'Invalid username or password.' });
 		}
